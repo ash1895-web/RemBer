112f6c091127f163c4e2a14776a257479f0392a7
ash1895-web <aseeransari79@gmail.com>
2024-02-03 18:17:12 +0530

    user model added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,18 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession()
-
-  if (session) {
+  
+  if (session) {    
+    console.log('session is present');
+    await fetch(`${process.env.BASE_URL}/api/users`, {
+      method: 'POST',
+      body: JSON.stringify(session.user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-store'
+    })
+    
     redirect('/dashboard')
   }
 }
