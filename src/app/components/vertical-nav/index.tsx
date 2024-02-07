5a1afe320d804857cb530fa16b4d5a3122af133a
ash1895-web <aseeransari79@gmail.com>
2024-02-08 02:34:27 +0530

    menu items added

--- src/app/components/vertical-nav/index.tsx
+++ src/app/components/vertical-nav/index.tsx
@@ -7,10 +7,10 @@ export default function VerticalNav(): JSX.Element {
     return (
         <div className={styles.verticalNavContainer}>
             <VerticalNavMenuItem title="Dashboard" iconName="dashboard"/>
-            <VerticalNavMenuItem title="Dashboard" iconName="dashboard"/>
-            <VerticalNavMenuItem title="Dashboard" iconName="dashboard"/>
-            <VerticalNavMenuItem title="Dashboard" iconName="dashboard"/>
-            <VerticalNavMenuItem title="Dashboard" iconName="dashboard"/>
+            <VerticalNavMenuItem title="Reminders" iconName="reminder"/>
+            <VerticalNavMenuItem title="Surveys" iconName="list_alt"/>
+            <VerticalNavMenuItem title="People" iconName="face"/>
+            <VerticalNavMenuItem title="Payments" iconName="payments"/>
         </div>
     )
 }
