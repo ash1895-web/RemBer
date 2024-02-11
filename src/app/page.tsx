82a715ff60db0e3f03f4f895a301252bd115a6bb
ash1895-web <aseeransari79@gmail.com>
2024-02-11 19:15:05 +0530

    fixed fetch request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,14 @@ export default async function Home() {
   const session = await getServerSession()
   
   if (session) {    
-    const res = await fetch(`${process.env.BASE_URL}/api/users`, {
+    await fetch(`${process.env.BASE_URL}/api/users`, {
       method: 'POST',
       body: JSON.stringify(session.user),
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${cookies().get('next-auth.session-token')}`,
+        'Authorization': `Bearer ${cookies().get('next-auth.session-token')?.value}`,
       }    
-    })
-   
+    })    
     redirect('/dashboard')
   }
 }
